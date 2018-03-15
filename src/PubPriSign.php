a0465e0050e3605e49ee35fbc0c5b116cc52eb04
wangzhenhong <wangzhenhong@pipaw.net>
2018-03-15 09:14:07 +0800

    [chg]add x509 pub key

--- src/PubPriSign.php
+++ src/PubPriSign.php
@@ -1,5 +1,7 @@
 <?php
+
 namespace Qqes\PPSign;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -15,18 +17,24 @@ class PubPriSign {
     //put your code here
 
     /**
-     * 得到新的私钥和公钥
+     * 
+     * @param boolean $pub_x509
+     * @param int $pub_exp_days
+     * @return array(x509_pub, pub , pri)
      */
-    public static function getNewPubAndPriKey() {
+    public static function getNewPubAndPriKey($x509_exp_days = 36500) {
         //create new private and public key
         $private_key_res = openssl_pkey_new(array(
             "private_key_bits" => 2048,
             "private_key_type" => OPENSSL_KEYTYPE_RSA,
         ));
         openssl_pkey_export($private_key_res, $pri_key);
+        $csr = openssl_csr_new(array(), $private_key_res);
+        $sscert = openssl_csr_sign($csr, null, $private_key_res, $x509_exp_days);
+        openssl_x509_export($sscert, $x509_pub_key);
         $pub_detail = openssl_pkey_get_details($private_key_res);
         $pub_key = $pub_detail['key'];
-        return [$pub_key, $pri_key];
+        return [$x509_pub_key, $pub_key, $pri_key];
     }
 
     /**
@@ -40,8 +48,7 @@ class PubPriSign {
         openssl_sign($data, $signature, $pri_key_res, "sha1WithRSAEncryption");
         return base64_encode($signature);
     }
-    
-    
+
     /**
      * 使用公钥验证签名
      * @param type $publicKey
@@ -49,7 +56,7 @@ class PubPriSign {
      * @param type $signature
      * @return type
      */
-    public static function checkSignWitPubKey($publicKey, $data,$signature){
+    public static function checkSignWitPubKey($publicKey, $data, $signature) {
         $public_key_res = openssl_pkey_get_public($publicKey);
         return (boolean) openssl_verify($data, base64_decode($signature), $public_key_res, OPENSSL_ALGO_SHA1);
     }
--- test.php
+++ test.php
@@ -9,12 +9,10 @@
 require 'src/PubPriSign.php';
 use Qqes\PPSign\PubPriSign;
 
-list($pub_key, $pri_key) = PubPriSign::getNewPubAndPriKey();
+list($x509_pub_key, $pub_key,  $pri_key) = PubPriSign::getNewPubAndPriKey(true);
 
 $msg = "Hello im a msg";
 
 $signature = PubPriSign::signByPriKey($pri_key, $msg);
-
-
 
 var_dump(PubPriSign::checkSignWitPubKey($pub_key, $msg, $signature));
